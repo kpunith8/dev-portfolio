2ee11fba18dc046086bc1e0ffdde6d6eef5a083a
Punith K <kpunith8@gmail.com>
2019-08-03 18:59:03 +0530

    Updates

---
+++ precache-manifest.a7b3eefb0d2e8a25b59260da52e2714e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e89426d87d8b00410c3098756c3ff75e",
+    "url": "/dev-portfolio/index.html"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/dev-portfolio/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "afbd0064c92cf2a1dd7b",
+    "url": "/dev-portfolio/static/js/main.2baa1ef5.chunk.js"
+  },
+  {
+    "revision": "88b939c8ada9983f3096",
+    "url": "/dev-portfolio/static/js/runtime~main.e91790c5.js"
+  }
+]);
