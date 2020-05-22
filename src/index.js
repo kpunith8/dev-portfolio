0ab9ea61ed360b402aafd326728266c48669526e
Punith K <kpunith8@gmail.com>
2020-05-22 21:25:21 +0530

    fixing import typo

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import MainPage from "./components/main-page/main-page";
+import MainPage from "./Components/main-page/main-page";
 
 ReactDOM.render(<MainPage />, document.getElementById("root"));
