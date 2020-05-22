326e23531e721590f72cc1fd9661cbcc761e4266
Punith K <kpunith8@gmail.com>
2020-05-22 21:27:24 +0530

    fixing import typo

--- src/Components/main-page/main-page.js
+++ src/Components/main-page/main-page.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { Container } from "@material-ui/core";
-import AboutMe from "../about-me/about-me";
-import Experience from "../experience/experience";
-import Education from "../education/education";
-import Skills from "../skills/skills";
-import ContactMe from "../contact-me/contact-me";
+import aboutMe from "../about-me/about-me";
+import Experience from "../Experience/Experience";
+import Education from "../Education/Education";
+import Skills from "../Skills/Skills";
+import contactMe from "../contact-me/contact-me";
 import "./main-page.css";
 
 const MainPage = () => (
